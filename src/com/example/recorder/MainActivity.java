041fc421b3334a391132c1ab52aa9c8886236d05
batai <batai0219@gmail.com>
2014-06-14 15:52:31 +0900

    3rd commit

--- src/com/example/recorder/MainActivity.java
+++ src/com/example/recorder/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.recorder;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract.Constants;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,6 +15,7 @@ import android.widget.Button;
 public class MainActivity extends Activity implements OnClickListener{
 	private Button movie;
 	private Button setting;
+	private Button folder;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,10 @@ public class MainActivity extends Activity implements OnClickListener{
         setting = (Button)findViewById(R.id.Setting);
         setting.setOnClickListener(this);
         
+        folder = (Button)findViewById(R.id.Folder);
+        folder.setOnClickListener(this);
         
+      
 	}
 
 	@Override
@@ -31,14 +40,25 @@ public class MainActivity extends Activity implements OnClickListener{
 		// TODO 自動生成されたメソッド・スタブ
 		switch (v.getId()) {
 		case R.id.Movie:
+			/*
 			Intent intent = new Intent(this,SampleView.class);
 			startActivity(intent);
-			break;
+			Log.e("click", "click");
+			*/
+			RecTimer mRecTimer=new RecTimer(getApplicationContext());
+			mRecTimer.setTimer(SampleView.class,10*1000,0);
 			
+			
+			break;
+		    
 		case R.id.Setting:
 			
 			break;
+			
+		case R.id.Folder:
+			Intent intent2 = new Intent(this,Thumbnail.class);
+			startActivity(intent2);
+			Log.e("click", "click");
 		}
 	}
-	
 }
