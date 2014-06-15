4308bedcb2d42829325bf7e6ab3e97f8c2513dc2
batai <batai0219@gmail.com>
2014-06-16 01:54:07 +0900

    4th commit

--- src/com/example/recorder/RecTimer.java
+++ src/com/example/recorder/RecTimer.java
@@ -1,41 +1,21 @@
 package com.example.recorder;
 
-import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 
-public class RecTimer{
-	private Context context;
-	
-	public RecTimer(Context context){
-		this.context=context;
+
+
+public class RecTimer extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO 自動生成されたメソッド・スタブ
+		intent.setClass(context,SampleView.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		Log.e("Intent","SampleView");
+		context.startActivity(intent);
 	}
-	
-	
-	public <T>void setTimer(Class<T> Activity,int looptime,int serviceID){
-		Intent intent =new Intent(context,Activity);
-		PendingIntent pendingIntent = PendingIntent.getService(context,serviceID,intent,PendingIntent.FLAG_CANCEL_CURRENT);
-		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		Calendar cal=Calendar.getInstance();
-		am.setRepeating(AlarmManager.RTC,cal.getTimeInMillis(),looptime,pendingIntent);
-		Log.e("AlarmManager","set");
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
